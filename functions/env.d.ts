72dc4283081d024cc495c0b844c334a01f58dbb1
lamlucas <168625798+lamlucas@users.noreply.github.com>
2026-05-08 19:26:00 +0700

    Add files via upload

---
+++ functions/env.d.ts
@@ -0,0 +1,9 @@
+export interface Env {
+  SPREADSHEET_ID_MAIN: string;
+  SPREADSHEET_ID_DEBT_SALES: string;
+  ADMIN_USERNAME: string;
+  ADMIN_PASSWORD: string;
+  SESSION_SECRET: string;
+  GOOGLE_SERVICE_ACCOUNT_JSON: string;
+  BALANCE_REVEAL_PASSWORD: string;
+}
